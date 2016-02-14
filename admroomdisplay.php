226c3fcc7e0e2b33904d6624485b53b5a8869edc
chinhang95 <noctisxxd@gmail.com>
2016-02-14 23:44:54 +0800

    Create admroomdisplay.php

---
+++ admroomdisplay.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+<?php
+include('config.php');
+$query1=mysql_query("SELECT * FROM room");
+echo "<table border='1'>
+<tr><td>Room ID</td><td>Room Type</td><td>Room Capacity</td><td>action</td><td>action</td>";
+while($query2=mysql_fetch_array($query1))
+{
+echo "<tr><td>".$query2['room_id']."</td>";
+echo "<td>".$query2['room_type']."</td>";
+echo "<td>".$query2['room_capacity']."</td>";
+echo "<td><a href='aredit.php?id=" .$query2['room_id']. " '>Edit</a></td>";
+echo "<td><a href='ardelete.php?id=" .$query2['room_id']. " '>Del</a></td></tr>";
+}
+?>
+<p><a href='aradd.php'>Add Room</a></p>
+</table>
+</body>
+</html>

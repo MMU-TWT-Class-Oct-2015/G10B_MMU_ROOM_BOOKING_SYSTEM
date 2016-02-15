2c159cc0521acf7ac669fb4f20f36e45c4433cd0
kaishaimiyuki <ezzahafraa@gmail.com>
2016-02-15 14:26:21 +0800

    Create stafviewrecords.php

---
+++ stafviewrecords.php
@@ -0,0 +1,48 @@
+<html>
+<head><title>Booking Records</title></head>
+<link rel="stylesheet" type="text/css" href="main.css">
+<body>
+  <ul class="header">
+   <li class="headerli"><a class="headera" href="stafviewreords.php">View Booking Records</a></li>
+    <li class="headerli"><a class="headera" href="logout.php">Log out</a></li>
+   </ul>
+  <?php
+include('config.php');
+$query="SELECT * FROM booking order by date_of_book";
+$data = mysql_query($query);
+?>
+
+<table border="1" bgcolor="white" align="center">
+<?php
+Print ("<tr>");
+print("<th>Booking ID</th>");
+print("<th>Date</th>");
+Print("<th>Time start</th>");
+Print("<th>Time end</th>");
+Print("<th>Organizer</th>");
+Print("<th>Num of participant</th>");
+Print("<th>User_ID</th>");
+Print("<th>Room ID</th>");
+print("</tr>");
+while ($row = mysql_fetch_row($data)){
+print ("<tr>");
+foreach ($row as $key => $value )
+print ("<td>$value</td>");
+print("</tr>");
+}
+ ?>
+ <table>
+   <h1 align="center">Book a room?</h1>
+   <form method= "POST" action ="viewavailable.php">
+      <p>Select your available booking room type:
+     <select name="select">
+       <option selected>Lecture Room</option>
+      <option>Tutorial Room</option>
+       <option>Main Hall</option>
+       <option>Exam Hall</option>
+       <option>Meeting Room</option>
+   </select></p>
+   <p><input type ="submit" value="Go"></p>
+   </form>
+ </body>
+ </html>
